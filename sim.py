b02e9c434fd718db11957c86e9422f4c4cf2a510
dddjewelsbbb <JMM337@pitt.edu>
2017-10-01 01:11:37 -0400

    Multiple starting points, minor refactor

--- sim.py
+++ sim.py
@@ -26,14 +26,22 @@ import random as rand
 
 # Transmission variables
 talkToTransmit = True   # Transmission = just talking?
-chance_to_spread = 0.01 # If transmit =/= talk, what is the chance upon talking
+chance_to_spread = 0.20 # If transmit =/= talk, what is the chance upon talking
 
 # Forgetting variables
 can_node_forget = True
-node_forget_chance = 0.01 # Chance for node to forget
+node_forget_chance = 0.005 # Chance for node to forget
 
-maximum_allowed_simulation_rounds = 1000 # 1 million runs currently
-num_runs = 10       # Default number of runs
+
+# Simulation arguments
+maximum_allowed_simulation_rounds = 100 # Max amount of rounds before we stop running a simulation
+num_runs = 3      # Default number of runs per simulation
+
+
+# Debug/logging
+LOGGING = True # Keep the user informed of what is going on, generally
+DEBUG = False # General debug info
+DEBUG_SEVERE = False # Output information at each step if it can be done
 
 
 ####################################################################################
@@ -45,18 +53,53 @@ def main():
    # Read in a graph
    simulation_graph = nx.read_graphml('simplemodel.graphml')
    
-   # Initialize the graph with attributes we need
-   init(simulation_graph, 'n10')
-   
-   # Run a simulation 100 times
-   simulate(simulation_graph, num_runs)
+   # Dispatch a simulation (multiple simulation runs with different nodes)
+   simulation_dispatcher(simulation_graph)
 
 ####################################################################################
 
 
-
-
-
+def simulation_dispatcher(graph):
+   total_rounds = 0
+   total_flagged = 0
+   total_max_flags = 0
+   total_fails = 0
+   total_simulations = 0
+   total_successes = 0
+   
+   # Start from every node in the graph
+   for n in graph.node:
+      graphcopy = copy.deepcopy(graph)
+      init(graphcopy, n)
+      flagged, max_flags, sum_rounds, num_fails = simulate(graphcopy, num_runs)
+      total_rounds += sum_rounds
+      total_flagged += flagged
+      total_max_flags += max_flags
+      total_fails += num_fails
+      total_simulations += num_runs
+      
+   total_successes = total_simulations-total_fails
+   
+   percent_finished = percent(total_successes, total_simulations)
+   percent_flagged = total_percent_flagged(graph, total_flagged, total_simulations)
+   
+   
+   
+   average_rounds = 0
+   if (total_successes > 0):
+      average_rounds = total_successes / float(num_simulations) * 100
+   print '\n' * 2
+   print '*' * 27
+   print 'Simulations complete.'
+   print '*' * 27
+   print 'Total successful simulations (spread across whole graph): ' + str(total_successes)
+   print 'Total failed simulations (could not spread across graph): ' + str(total_fails)
+   print 'Total number of simulations (complete and incomplete):    ' + str(total_simulations)
+   print '\n'
+   print 'Average rounds until completion (across ' + str(total_successes) +' simulation runs): ' + str(average_rounds)
+   print 'Average graph completion rate (across all graphs): ' + str(percent_finished) + '%'
+   print 'Average graph spread rate (across all graphs): ' + str(percent_flagged) + '%'
+      
 ####################################################################################
 # Simulation function - to be changed and altered. Highly volatile.                #
 ####################################################################################
@@ -66,33 +109,45 @@ def simulate(graph, num_simulations):
    num_fails = 0
    current_run = 1
    total_flagged = 0
+   
    while (current_run <= num_simulations):
       graph_instance = copy.deepcopy(graph)
       run_time,num_flagged = run(graph_instance, weight_max)
       total_flagged += num_flagged
+	  
+	  # WARNING: Incomplete graphs return -1, so no sum_time is added
       if (run_time < 0):
-         print 'Run ' + str(current_run) + ' failed! ' + str(num_flagged) + '/' + str(len(graph.node)) + ' flagged. (' + str(successPercent(graph, num_flagged)) + '% complete)'
+         if (LOGGING or DEBUG or DEBUG_SEVERE):
+            print 'Run ' + str(current_run) + ' failed! ' + str(num_flagged) + '/' + str(len(graph.node)) + ' flagged. (' + str(percent_flagged(graph, num_flagged)) + '% complete)'
          num_fails += 1
       else:
-         print 'Run ' + str(current_run) + ' took ' + str(run_time) + ' rounds. ' + str(len(graph.node)) + '/' + str(len(graph.node)) + ' flagged. (100% complete)'
+         if (LOGGING or DEBUG or DEBUG_SEVERE):
+            print 'Run ' + str(current_run) + ' took ' + str(run_time) + ' rounds. ' + str(len(graph.node)) + '/' + str(len(graph.node)) + ' flagged. (100% complete)'
          sum_time += run_time
 
       current_run += 1
    if (num_simulations == num_fails): # Avoid division by 0
-      print 'All runs failed. maximum_allowed_simulation_rounds = ' + str(maximum_allowed_simulation_rounds) + ' (Average completion rate: ' + str(totalSuccessPercent(graph, total_flagged, num_simulations)) + '%)'
+      print 'All runs failed. maximum_allowed_simulation_rounds = ' + str(maximum_allowed_simulation_rounds) + ' (Average completion rate: ' + str(total_percent_flagged(graph, total_flagged, num_simulations)) + '%)'
    else:
       print str(num_simulations) + ' simulations finished. ' + str(num_fails) + ' simulations failed. Average run time: ' + str(sum_time/ (num_simulations - num_fails) ) + ' rounds'
-      print 'Average completion rate: ' + str(totalSuccessPercent(graph, total_flagged, num_simulations)) + '%)'
+      print 'Average completion rate: ' + str(total_percent_flagged(graph, total_flagged, num_simulations)) + '%)'
+
+   return total_flagged, len(graph.node) * num_simulations, sum_time, num_fails
 ####################################################################################
 
 
-
+####################################################################################
+# Converts a numerator and a denominator into a percentage.                        #
+####################################################################################
+def percent(numerator, denominator):
+   return 100 * numerator / float(denominator)
+####################################################################################
 
 ####################################################################################
-# Gets a percentage of success based on a graph node count and the number of       #
+# Gets a percentage of flagged nodes on the graph handed into it.                  #
 # nodes that were flagged.                                                         #
 ####################################################################################
-def successPercent(graph, num_flagged):
+def percent_flagged(graph, num_flagged):
    return 100 * num_flagged / float(len(graph.node))
 ####################################################################################
 
@@ -102,7 +157,7 @@ def successPercent(graph, num_flagged):
 # Returns the total percentage of success given a graph, the number of flagged     #
 # nodes and the total number of simulations.                                       #
 ####################################################################################
-def totalSuccessPercent(graph, num_flagged, num_simulations):
+def total_percent_flagged(graph, num_flagged, num_simulations):
    return 100 * num_flagged / float((len(graph.node) * num_simulations))
 ####################################################################################
 
@@ -146,7 +201,8 @@ def max_weight(graph):
    dict = nx.get_edge_attributes(graph, 'weight')
    for val in dict:
       max_weight = max(max_weight, dict[val])
-   #print max_weight
+   if (DEBUG_SEVERE):
+      print '[DEBUG]: max_weight of ' + str(graph) + ' is ' + str(max_weight)
    return max_weight
 ####################################################################################
 
@@ -205,17 +261,16 @@ def round(graph, round_num, max_weight):
          for g in graphcopy.edge[n]:
             # If the graph node in both the copy and original aren't flagged
             if (not graphcopy.node[g]['flagged'] and not graph.node[g]['flagged']):
-               #print '!!! ' + g + ' is flagged'    #  Debug
-               #print 'graph.edge[' + str(n) + ']: ' + str(graph.edge[n])
-               
-               # To output graph edge weights - I have no tests yet
-               #print 'Graph edge weight: ' + str(graph.edge[n][g]['weight'])
-               
+               if (DEBUG_SEVERE):
+                  print '[DEBUG]: graph and graphcopy of node ' + str(g) +  ' unflagged...'
                if (will_spread(n, g, graph, max_weight)):
-                  graph.node[g]['flagged'] = True
+                  graph.node[g]['flagged'] = True		  
                   # Increment the number of given_flags this round
                   given_flags += 1
-                  #print '[' + g + ']: ' + str(graph.node[g]['flagged']
+               if (DEBUG_SEVERE):
+                  print '[DEBUG]: ...flagged node ' + str(g)
+            elif (DEBUG_SEVERE): # Output no change has been made
+               print '[DEBUG]: ...no action taken.'
    return given_flags-forgot_flags # TODO: return forgot flags and given flags
 ####################################################################################
 
@@ -231,7 +286,7 @@ def num_flagged(graph):
       if (nodes[val]):
          num_flagged += 1
    return num_flagged
-################################################################
+####################################################################################
 
 
 
